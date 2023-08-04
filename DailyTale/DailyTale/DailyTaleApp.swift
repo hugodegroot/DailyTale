3d6f48aa7801de61174e2099d51040be6dd12ba3
Hugo De Groot <hugodegroot97@gmail.com>
2023-08-04 21:52:38 +0200

    Initial commit

---
+++ DailyTale/DailyTale/DailyTaleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyTaleApp.swift
+//  DailyTale
+//
+//  Created by Hugo de Groot on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct DailyTaleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
